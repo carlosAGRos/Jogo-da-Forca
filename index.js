9e2027015785895b68f1cbb48a4bcfaf5a0a7d9c
Gustavo Araújo Mendes <gustavoraujo12@gmail.com>
2026-08-28 16:51:12 -0300

    feat: adiciona a arte da forca no jogo

--- index.js
+++ index.js
@@ -14,13 +14,26 @@ async function iniciarJogo() {
     let jogoRodando = true;
 
     let vidas = 6; 
-
+    const arteForca =[
+        " +---+\n | |\n O |\n /|\\ |\n / \\ |\n ", // 0 vidas
+        " +---+\n | |\n O |\n /|\\ |\n /  |\n ", // 1 vida
+        " +---+\n | |\n O |\n /|\\ |\n   |\n ", // 2 vidas
+        " +---+\n | |\n O |\n /| |\n   |\n ", // 3 vidas
+        " +---+\n | |\n O |\n | |\n   |\n ", // 4 vidas
+        " +---+\n | |\n O |\n  |\n   |\n ", // 5 vidas
+        " +---+\n | |\n  |\n  |\n   |\n "  // 6 vidas
+    ]
     console.log ("=== Bem-vindo ao jogo da Forca ===");
+    
 
     while(jogoRodando) {
+        
+        console.log(`\nVidas Restantes: <3 ${vidas}`);
+        console.log(arteForca[vidas]);
+
        console.log(`\nPalavra atual: ${letrasDescobertas.join(" ")}`); 
 
-        const chute = (await rl.question("Digite uma letra: ")).toUpperCase();
+        const chute = (await rl.question("Digite uma le tra: ")).toUpperCase();
         let acertou = false;
 
         for(let i = 0; i < palavraSecreta.length; i++) {
